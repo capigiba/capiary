a5c117ace5b1cdb911fd65462418c9c621e4ba17
capigiba <21127017@student.hcmus.edu.vn>
2025-04-11 13:57:40 +0700

    fix(entity): adjust _id

--- internal/domain/entity/blog_post_entity.go
+++ internal/domain/entity/blog_post_entity.go
@@ -4,7 +4,7 @@ import "time"
 
 // BlogPost describes the top-level information for a blog post.
 type BlogPost struct {
-	ID        int       `json:"id"`
+	ID        int       `json:"_id" bson:"_id"`
 	Title     string    `json:"title"`
 	Blocks    []Block   `json:"blocks"`
 	CreatedAt time.Time `json:"created_at"`
