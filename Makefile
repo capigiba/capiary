43bb9d79b06e75e684e0ebc31cbae2a7be5b4532
capigiba <21127017@student.hcmus.edu.vn>
2025-04-11 13:59:50 +0700

    feat(server): run server

---
+++ Makefile
@@ -0,0 +1,49 @@
+# Variables
+CMD_DIR := cmd/server
+SETUP_DIR := cmd/setup
+APP_NAME := capiary
+SCRIPT_DIR := scripts
+INITIALIZE_DIR := internal/initialize
+
+# Default target
+all: build
+
+# Run the application
+run:
+	go run $(CMD_DIR)/main.go
+
+setup:
+	go run $(SETUP_DIR)/main.go
+
+# Generate wire dependencies
+wire:
+	go install github.com/google/wire/cmd/wire@latest
+	cd $(INITIALIZE_DIR) && wire
+
+# Build the application
+build:
+	cd $(CMD_DIR) && go build -o $(APP_NAME)
+
+# Clean the generated binaries
+clean:
+	rm -f $(CMD_DIR)/$(APP_NAME)
+
+# Swagger
+swag:
+	go install github.com/swaggo/swag/cmd/swag@latest
+	swag init -g $(CMD_DIR)/main.go -o ./docs
+
+# Run all steps from the script
+run-all:
+	bash $(SCRIPT_DIR)/run-all.sh
+
+# Help
+help:
+	@echo "Makefile for $(APP_NAME)"
+	@echo
+	@echo "Usage:"
+	@echo "  make run         Run the application"
+	@echo "  make swag		  Run the swagger"
+	@echo "  make build       Build the application"
+	@echo "  make clean       Clean the generated binaries"
+	@echo "  make help        Show this help message"
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,79 @@
 package main
+
+import (
+	"os"
+
+	"github.com/capigiba/capiary/internal/config"
+	handler "github.com/capigiba/capiary/internal/handler/rest/v1"
+	"github.com/capigiba/capiary/internal/infra/db/mongodb"
+	"github.com/capigiba/capiary/internal/repositories"
+	"github.com/capigiba/capiary/internal/router"
+	"github.com/capigiba/capiary/internal/services"
+	"github.com/capigiba/capiary/pkg/logger"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	appLogger := logger.NewLogger("Initialize")
+
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		appLogger.Errorf("config loading error: %w", err)
+		os.Exit(1)
+	}
+
+	// dbPostgresConn, err := postgres.NewPostgresDB(cfg.Database.PostgresURL)
+	// if err != nil {
+	// 	appLogger.Errorf("database initialization error: %w", err)
+	// 	os.Exit(1)
+	// }
+
+	dbMongoConn := mongodb.NewMongoDBClient(cfg.Database.MongodbURI)
+
+	// userRepo := repositories.NewUserRepo(dbPostgresConn)
+	// authUserMiddleware := middleware.NewAuthUserMiddleware(userRepo, cfg.Server.JWTSecret)
+	// userService := services.NewUserService(userRepo, authUserMiddleware)
+	// userHandler := handler.NewUserHandler(userService)
+
+	blogRepo := repositories.NewBlogPostRepository(dbMongoConn)
+	blogService := services.NewBlogPostService(blogRepo)
+	blogHandler := handler.NewBlogPostHandler(blogService)
+
+	swaggerRouter := router.NewSwaggerRouter()
+
+	// appRouter := router.NewAppRouter(userHandler, blogHandler, authUserMiddleware, swaggerRouter)
+	appRouter := router.NewAppRouter(nil, blogHandler, nil, swaggerRouter)
+
+	router := gin.Default()
+
+	// Apply CORS middleware with configured settings
+	corsConfig := cfg.CORS
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     corsConfig.AllowedOrigins,
+		AllowMethods:     corsConfig.AllowedMethods,
+		AllowHeaders:     corsConfig.AllowedHeaders,
+		ExposeHeaders:    corsConfig.ExposeHeaders,
+		AllowCredentials: corsConfig.AllowCredentials,
+		MaxAge:           corsConfig.MaxAge,
+	}))
+
+	apiGroup := router.Group("/api")
+	registerAPIRoutes(apiGroup, appRouter)
+	registerSwaggerRoutes(router, appRouter)
+
+	port := cfg.Server.Port
+	if err := router.Run(":" + port); err != nil {
+		appLogger.Errorf("Failed to start the server: %v", err)
+		os.Exit(1)
+	}
+}
+
+func registerAPIRoutes(group *gin.RouterGroup, appRouter *router.AppRouter) {
+	appRouter.RegisterBlogRoutes(group)
+}
+
+func registerSwaggerRoutes(router *gin.Engine, appRouter *router.AppRouter) {
+	swaggerGroup := router.Group("/")
+	appRouter.RegisterSwaggerRoutes(swaggerGroup)
+}
